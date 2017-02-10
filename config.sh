6733e1c18f5b5aa7418500864208a283cd8ae907
CKnappe <a@a.a>
2017-02-10 17:37:44 +0100

    Update

---
+++ config.sh
@@ -0,0 +1,8 @@
+git submodule update --init --recursive
+mkdir -p ~/.config
+rm -r ~/.config/awesome
+ln -s ~/.dotfiles/awesome ~/.config/awesome
+rm -r ~/.emacs.d
+ln -s ~/.dotfiles/.emacs.d ~/.emacs.d
+sudo pacman -S luarocks
+sudo luarocks install luafilesystem
